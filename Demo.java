1142f12c333a0e6b7048529b235229168d7033bb
SUNSAHAR20 <sunsahar14@gmail.com>
2023-03-01 11:25:28 -0400

    initial commit

---
+++ Demo.java
@@ -0,0 +1,6 @@
+public class Demo {
+    public static void main(String[] args) {
+        var twitterAPI = new TwitterAPI("appKey", "secret");
+        var tweets = twitterAPI.getRecentTweets();
+    }
+}
